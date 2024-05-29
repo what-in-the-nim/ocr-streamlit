5a19487095c20fa379d00efb4cf49554ddc12880
Win <chatcharinsang@gmail.com>
2024-05-29 23:31:16 +0700

    Change to zip input

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-import streamlit as st
-import pandas as pd
-from pathlib import Path
 import base64
+import io
+import os.path as op
+import zipfile
+from functools import partial
 
-def image_to_data_url(image_path: str) -> str:
+import pandas as pd
+import streamlit as st
+
+
+def image_to_data_url(zip_file: zipfile.ZipFile, image_path: str) -> str:
     """Open an image and convert to base64 data url."""
-    # Read image content.
-    with open(image_path, "rb") as image_file:
+    # Read image content in zip file.
+    with zip_file.open(image_path) as image_file:
         encoded_image = base64.b64encode(image_file.read()).decode("utf-8")
     # Get image path extension.
     image_extension = image_path.split(".")[-1]
@@ -14,79 +19,97 @@ def image_to_data_url(image_path: str) -> str:
     data_url = f"data:image/{image_extension};base64,{encoded_image}"
     return data_url
 
-def main():
-    st.title("CSV/TSV File Editor")
 
-    # File upload
-    file = st.file_uploader("Upload a CSV or TSV file", type=["csv", "tsv"])
+st.title("Label Editor")
 
-    if file is None:
-        st.stop()
+# File upload
+file = st.file_uploader("Upload a CSV or TSV file", type=["zip"])
 
-    # Get the path to image
-    path_to_image = st.text_input("Path to image column")
-    # Do not continue if path_to_image is empty
-    if not path_to_image:
-        st.stop()
+# If no file is uploaded, stop the script
+if file is None:
+    st.stop()
 
-    # Check if the path exists
-    path = Path(path_to_image)
-    if not path.exists():
-        st.error(f"Path does not exist: {path}")
-        st.stop()
+# If file is not a zip file, stop the script
+if not file.name.endswith(".zip"):
+    st.error("Invalid file format. Please upload a ZIP file.")
+    st.stop()
 
-    # Read the file
-    if file.name.endswith('.csv'):
-        sep = ','
-    elif file.name.endswith('.tsv'):
-        sep = '\t'
-    else:
-        st.error("Invalid file format. Please upload a CSV or TSV file.")
-        st.stop()
+# Unzip the file to buffer
+zip_file = zipfile.ZipFile(file)
+files = zip_file.namelist()
+# Remove all __MACOSX and .DS_Store files
+files = [file for file in files if "__MACOSX" not in file and ".DS_Store" not in file]
 
-    # Load the data
-    df = pd.read_csv(file, sep=sep)
+st.write("Files in the ZIP file:")
+st.write(files)
 
-    # Check if the file has a path and text column
-    if not {"path", "text"}.issubset(df.columns):
-        st.error("The file must have 'path' and 'text' columns, but got: " + ", ".join(df.columns))
-        st.stop()
-        
-    # Create a temporary path column
-    df["image"] = df["path"].apply(lambda x: path.joinpath(x).as_posix())
-    # Create a base64 data url column
-    df["image"] = df["image"].apply(image_to_data_url)
-    # Fill NaN with empty string on text column
-    df["text"] = df["text"].fillna("")
+# Find the tsv file or csv file
+label_files = [file for file in files if file.endswith(".tsv") or file.endswith(".csv")]
+if len(label_files) != 0:
+    # Select the file
+    label_file = st.selectbox("Select a file", label_files)
+else:
+    # Use the first file
+    label_file = label_files[0]
 
-    # Display DataFrame
-    edited_df = st.data_editor(
-        data=df,
-        use_container_width=True,
-        num_rows="dynamic",
-        column_order=["image", "text"],
-        height=500,
-        disabled=("image",),
-        hide_index=False,
-        column_config={
-            "image": st.column_config.ImageColumn(width="medium", label="Image Preview", help="Image Preview"),
-            "text": st.column_config.TextColumn(label="Text", help="Label of the image."),
-        },
+# Get the file content from the zip file
+with zip_file.open(label_file) as f:
+    label_content = f.read()
+    label_bytes = io.BytesIO(label_content)
+
+# Read label file
+sep = "\t" if label_file.endswith(".tsv") else ","
+df = pd.read_csv(label_bytes, sep=sep)
+
+# Check if the file has a path and text column
+if not {"path", "text"}.issubset(df.columns):
+    st.error(
+        "The file must have 'path' and 'text' columns, but got: "
+        + ", ".join(df.columns)
     )
-    # Remove the image column from the edited DataFrame
-    edited_df.drop(columns=["image"], inplace=True)
+    st.stop()
 
-    # Edited filename
-    current_time = pd.Timestamp.now().strftime("%Y%m%d_%H%M%S")
-    save_filename = file.name.replace(".", f"_{current_time}.")
+# Use commonpath as root.
+root_dir = op.commonpath(files)
 
-    # Download button
-    st.download_button(
-        label="Download",
-        data=edited_df.to_csv(sep="\t", index=False),
-        file_name=save_filename,
-        mime="text/csv"
-    )
+# Create a partial function to convert image to data url
+image_to_data_url = partial(image_to_data_url, zip_file)
 
-if __name__ == "__main__":
-    main()
+# Create a temporary path column
+df["image"] = df["path"].apply(lambda x: op.join(root_dir, x))
+# Create a base64 data url column
+df["image"] = df["image"].apply(image_to_data_url)
+# Fill NaN with empty string on text column
+df["text"] = df["text"].fillna("")
+
+# Display DataFrame
+edited_df = st.data_editor(
+    data=df,
+    use_container_width=True,
+    num_rows="dynamic",
+    column_order=["image", "text"],
+    height=500,
+    disabled=("image",),
+    hide_index=False,
+    column_config={
+        "image": st.column_config.ImageColumn(
+            width="medium", label="Image Preview", help="Image Preview"
+        ),
+        "text": st.column_config.TextColumn(label="Text", help="Label of the image."),
+    },
+)
+# Remove the image column from the edited DataFrame
+edited_df.drop(columns=["image"], inplace=True)
+
+# Edited filename
+current_time = pd.Timestamp.now().strftime("%Y%m%d_%H%M%S")
+label_filename = op.basename(label_file)
+save_filename = label_filename.replace(".", f"_{current_time}.")
+
+# Download button
+st.download_button(
+    label="Download",
+    data=edited_df.to_csv(sep="\t", index=False),
+    file_name=save_filename,
+    mime="text/csv",
+)
